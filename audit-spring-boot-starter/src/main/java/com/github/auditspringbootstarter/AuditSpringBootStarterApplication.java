081510667ab6609f2728b93acd4a7f88110480e1
shishaodong <shishaodong@cmhi.chinamobile.com>
2020-01-08 19:45:40 +0800

    Initial commit

---
+++ audit-spring-boot-starter/src/main/java/com/github/auditspringbootstarter/AuditSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.github.auditspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditSpringBootStarterApplication.class, args);
+	}
+
+}
