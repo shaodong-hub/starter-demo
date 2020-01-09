d0b3db2db03977c124fe5c7429674555e9828066
shishaodong <shishaodong@cmhi.chinamobile.com>
2020-01-09 16:45:26 +0800

    完成DEMO

--- spring-boot-demo/src/main/java/com/github/springbootdemo/SpringBootDemoApplication.java
+++ spring-boot-demo/src/main/java/com/github/springbootdemo/SpringBootDemoApplication.java
@@ -1,16 +1,20 @@
 package com.github.springbootdemo;
 
-import com.github.auditspringbootstarter.annotation.EnableAudit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+/**
+ * https://app.yinxiang.com/fx/562e0616-ff2b-4ae3-8046-ca6527669f17
+ *
+ * @author shao
+ */
 //@EnableAudit
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
 
 }
